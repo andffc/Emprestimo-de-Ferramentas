21fd712bbea00448cb043bdffbf087a2066c1aa8
1072414468 <1072414468@UNS11DT099121.animaedu.intranet>
2024-05-21 19:36:49 -0300

    fix: Tirei o metodo getConexao para nao ter mais repetição na classe FerramentaDAO.

--- src/main/java/dao/FerramentaDAO.java
+++ src/main/java/dao/FerramentaDAO.java
@@ -1,27 +1,34 @@
 package dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import modelo.Ferramenta;
-import java.sql.*;
 
 public class FerramentaDAO {
-    
-    public ArrayList<Ferramenta> ListaFerramentas = new ArrayList<>();
 
+    public ArrayList<Ferramenta> ListaFerramentas = new ArrayList<>();
+    
+    private ConexaoDAO connect;
+    
     public ArrayList<Ferramenta> getMinhaLista() {
 
         ListaFerramentas.clear();
 
         try {
-            Statement stmt = this.getConexao().createStatement();
+            Statement stmt = connect.getConexao().createStatement();
             ResultSet res = stmt.executeQuery("SELECT * FROM tb_ferramentas");
             while (res.next()) {
 
-                int id = res.getInt("id_ferramentas");
+                int id = res.getInt("id_ferramenta");
                 String nome = res.getString("nome");
                 String marca = res.getString("marca");
-                double custoAquisicao = Double.parseDouble(res.getString("custoAquisicao"));
+                double custoAquisicao = Double.parseDouble(res.getString("custo_aquisicao"));
+                int idEmp = res.getInt("id_emprestimo");
 
-                Ferramenta objeto = new Ferramenta(id, nome, marca, custoAquisicao);
+                Ferramenta objeto = new Ferramenta(nome, marca, custoAquisicao, id, idEmp);
 
                 ListaFerramentas.add(objeto);
             }
@@ -36,8 +43,8 @@ public class FerramentaDAO {
     public int maiorId() {
         int maiorId = 0;
         try {
-            Statement stmt = this.getConexao().createStatement();
-            ResultSet res = stmt.executeQuery("SELECT MAX(id_ferramentas) id FROM tb_ferramentas");
+            Statement stmt = connect.getConexao().createStatement();
+            ResultSet res = stmt.executeQuery("SELECT MAX(id_ferramenta) id FROM tb_ferramentas");
             res.next();
             maiorId = res.getInt("id");
             stmt.close();
@@ -46,40 +53,11 @@ public class FerramentaDAO {
         }
         return maiorId;
     }
-    public Connection getConexao() {
 
-        Connection connection = null;
+    public boolean inserirFerramentaBD(Ferramenta objeto) {
+        String sql = "INSERT INTO tb_ferramentas(id_ferramenta,nome,marca,custo_aquisicao) VALUES(?,?,?,?)";
         try {
-            String driver = "com.mysql.cj.jdbc.Driver";
-            Class.forName(driver);
-
-            String server = "localhost";
-            String database = "Database";
-            String url = "jdbc:mysql://" + server + ":3306/" + database + "?useTimezone=true&serverTimezone=UTC";
-            String user = "myuser";
-            String password = "myuser";
-
-            connection = DriverManager.getConnection(url, user, password);
-
-            if (connection != null) {
-                System.out.println("Status: Conectado!");
-            } else {
-                System.out.println("Status: Não Conectado!");
-            }
-            return connection;
-
-        } catch (ClassNotFoundException e) {
-            System.out.println("O driver não foi encontrado.");
-            return null;
-        } catch (SQLException e) {
-            System.out.println("Não foi possível conectar...");
-            return null;
-        }
-    }
-   public boolean inserirFerramentaBD(Ferramenta objeto) {
-        String sql = "INSERT INTO tb_ferramentas(id_ferramentas,nome,marca,custoAquisicao) VALUES(?,?,?,?)";
-        try {
-            PreparedStatement stmt = this.getConexao().prepareStatement(sql);
+            PreparedStatement stmt = connect.getConexao().prepareStatement(sql);
 
             stmt.setInt(1, objeto.getId());
             stmt.setString(2, objeto.getNome());
@@ -98,9 +76,9 @@ public class FerramentaDAO {
 
     public boolean apagarFerramentaBD(int id) {
         try {
-            Statement stmt = this.getConexao().createStatement();
+            Statement stmt = connect.getConexao().createStatement();
 
-            stmt.executeUpdate("DELETE FROM tb_ferramentas WHERE id_ferramentas =" + id);
+            stmt.executeUpdate("DELETE FROM tb_ferramentas WHERE id_ferramenta =" + id);
 
             stmt.close();
         } catch (SQLException erro) {
@@ -110,9 +88,9 @@ public class FerramentaDAO {
     }
 
     public boolean alterarFerramentaBD(Ferramenta objeto) {
-        String sql = "UPDATE tb_ferramentas set nome = ?, marca = ?, custoAquisicao = ? WHERE id_ferramentas = ?";
+        String sql = "UPDATE tb_ferramentas set nome = ?, marca = ?, custo_aquisicao = ? WHERE id_ferramenta = ?";
         try {
-            PreparedStatement stmt = this.getConexao().prepareStatement(sql);
+            PreparedStatement stmt = connect.getConexao().prepareStatement(sql);
 
             stmt.setString(1, objeto.getNome());
             stmt.setString(2, objeto.getMarca());
@@ -134,14 +112,15 @@ public class FerramentaDAO {
         Ferramenta objeto = new Ferramenta();
         objeto.setId(id);
         try {
-            Statement stmt = this.getConexao().createStatement();
+            Statement stmt = connect.getConexao().createStatement();
 
-            ResultSet res = stmt.executeQuery("SELECT * FROM tb_ferramentas WHERE id_ferramentas = " + id);
+            ResultSet res = stmt.executeQuery("SELECT * FROM tb_ferramentas WHERE id_ferramenta = " + id);
             res.next();
 
             objeto.setNome(res.getString("nome"));
             objeto.setMarca(res.getString("marca"));
-            objeto.setCustoAquisicao(res.getDouble("custoAquisicao"));
+            objeto.setCustoAquisicao(res.getDouble("custo_aquisicao"));
+            objeto.setIdEmp(res.getInt("id_emprestimo"));
 
             stmt.close();
         } catch (SQLException erro) {
