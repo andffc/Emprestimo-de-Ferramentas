76aae82899263f73ceb1a280854b3b20f595cccb
andls <andls@Dezin>
2024-05-27 21:11:11 -0300

    feat: Criei o metodo para o verificar emprestimos ativos e pendentes.

--- src/main/java/dao/EmprestimoDAO.java
+++ src/main/java/dao/EmprestimoDAO.java
@@ -11,7 +11,9 @@ import modelo.Emprestimo;
 public class EmprestimoDAO {
 
     public static ArrayList<Emprestimo> ListaEmprestimos = new ArrayList<>();
-    
+
+    public ArrayList<Emprestimo> ListaEmprestimosAtivos = new ArrayList<>();
+
     private ConexaoDAO connect;
 
     public ArrayList<Emprestimo> getMinhaLista() {
@@ -54,7 +56,7 @@ public class EmprestimoDAO {
         }
         return maiorId;
     }
-    
+
     public boolean inserirEmprestimoBD(Emprestimo objeto) {
         String sql = "INSERT INTO tb_emprestimos(id_emprestimo,id_amigo,data_emprestimo, data_devolucao, entregue) VALUES(?,?,?,?,?)";
         try {
@@ -62,8 +64,8 @@ public class EmprestimoDAO {
 
             stmt.setInt(1, objeto.getId());
             stmt.setInt(2, objeto.getIdAmg());
-            stmt.setDate(3, objeto.getDataDevolucao());
-            stmt.setDate(4, objeto.getDataEmprestimo());
+            stmt.setDate(3, objeto.getDataEmprestimo());
+            stmt.setDate(4, objeto.getDataDevolucao());
             stmt.setBoolean(5, objeto.isEntregue());
 
             stmt.execute();
@@ -90,15 +92,13 @@ public class EmprestimoDAO {
     }
 
     public boolean alterarEmprestimoBD(Emprestimo objeto) {
-        String sql = "UPDATE tb_emprestimos set id_amigo = ?, data_emprestimo = ?, data_devolucao = ?, entregue = ? WHERE id_emprestimo = ?";
+        String sql = "UPDATE tb_emprestimos set data_devolucao = ?, entregue = ? WHERE id_emprestimo = ?";
         try {
             PreparedStatement stmt = connect.getConexao().prepareStatement(sql);
 
-            stmt.setInt(1, objeto.getIdAmg());           
-            stmt.setDate(2, objeto.getDataEmprestimo());
-            stmt.setDate(3, objeto.getDataDevolucao());
-            stmt.setBoolean(4, objeto.isEntregue());
-            stmt.setInt(5, objeto.getId());
+            stmt.setDate(1, objeto.getDataDevolucao());
+            stmt.setBoolean(2, objeto.isEntregue());
+            stmt.setInt(3, objeto.getId());
 
             stmt.execute();
             stmt.close();
@@ -130,5 +130,53 @@ public class EmprestimoDAO {
             System.out.println("Erro:" + erro);
         }
         return objeto;
+    }
+
+    public ArrayList<Emprestimo> getEmprestimosAtivos() {
+
+        ListaEmprestimosAtivos.clear();
+
+        try {
+            Statement stmt = connect.getConexao().createStatement();
+            ResultSet res = stmt.executeQuery("SELECT * FROM tb_emprestimos WHERE entregue is false");
+            while (res.next()) {
+
+                int id = res.getInt("id_emprestimo");
+                Date data_emp = res.getDate("data_emprestimo");
+                Date data_dev = res.getDate("data_devolucao");
+                boolean entregue = res.getBoolean("entregue");
+                int idEmp = res.getInt("id_amigo");
+
+                Emprestimo objeto = new Emprestimo(data_emp, data_dev, entregue, id, idEmp);
+
+                ListaEmprestimosAtivos.add(objeto);
+            }
+            stmt.close();
+
+        } catch (SQLException ex) {
+            System.out.println("Erro:" + ex);
+        }
+        return ListaEmprestimosAtivos;
+    }
+    
+    public boolean verificarPendencia(int id) {
+
+        try {
+            Statement stmt = connect.getConexao().createStatement();
+            ResultSet res = stmt.executeQuery("select entregue from tb_emprestimos;");
+            while (res.next()) {
+
+                boolean  entregue = res.getBoolean("entregue");
+
+                if (entregue == false ) {
+                    return true;
+                }
+            }
+            stmt.close();
+
+        } catch (SQLException ex) {
+            System.out.println("Erro:" + ex);
+        }
+        return false;
     }
 }
